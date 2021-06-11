594d0b006d340ff0b3de163881b9e26954350eb7
martline1 <yael.alcalla@gmail.com>
2021-06-11 11:24:09 -0500

    Completed project

--- main.py
+++ main.py
@@ -4,22 +4,41 @@ import cv2
 import numpy as np
 from skimage import io, img_as_float
 
-def main():
-	img = img_as_float(io.imread("Lola.jpeg", as_gray = True))
+def gaussTransform(imgPath, show = False, title = ""):
+	img = img_as_float(io.imread(imgPath, as_gray = True))
 
-	# Gauss
-	img_gauss = cv2.GaussianBlur(img, (5, 5), 5, borderType = cv2.BORDER_CONSTANT)
+	gauss = cv2.GaussianBlur(img, (5, 5), 5, borderType = cv2.BORDER_CONSTANT)
 
-	# Laplace
-	laplace = cv2.Laplacian(img_gauss, cv2.CV_64F, ksize = 5)
+	if show:
+		cv2.imshow(title, gauss)
+	else:
+		return gauss
 
-	# Canny necesita la imagen con el filtro laplace y dos thresholds para X y Y
+def laplaceTransform(imgPath, show = False, title = ""):
+	gauss = gaussTransform(imgPath)
+
+	laplace = cv2.Laplacian(gauss, cv2.CV_64F, ksize = 5)
+
+	if show:
+		cv2.imshow(title, laplace)
+	else:
+		return laplace
+
+def cannyTransform(title, imgPath):
+	laplace = laplaceTransform(imgPath)
+
 	canny = cv2.Canny(np.uint8(laplace), 100, 200)
 
-	cv2.imshow("Original", img)
-	cv2.imshow("Gauss", img_gauss)
-	cv2.imshow("Laplace", laplace)
-	cv2.imshow("Canny", canny)
+	cv2.imshow(title, canny)
+
+def main():
+	gaussTransform("Ajolote.jpeg", title = "Ajolote sin Ruido", show = True)
+	laplaceTransform("chapala.jpeg", title = "Chapala líneas", show = True)
+	laplaceTransform("Chucky.jpeg", title = "Chucky líneas", show = True)
+	gaussTransform("Lola.jpeg", title = "Lola sin ruido", show = True)
+	laplaceTransform("Paloma.jpeg", title = "Paloma líneas", show = True)
+	gaussTransform("pez.jpeg", title = "Pez sin ruido", show = True)
+	cannyTransform("Serpiente laplace y Canny", "serpiente.jpeg")
 
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
